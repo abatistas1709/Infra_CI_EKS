403e4bae78960e85d3b0d018683aaae5bfecb6b4
Alexandre Silva <abatistas1709@gmail.com>
2026-01-21 17:29:41 -0300

    Remove deployment

--- infra/kubernetes.tf
+++ infra/kubernetes.tf
@@ -1,25 +1,3 @@
-# Aplicação
-resource "kubernetes_deployment" "go-api" {
-  metadata { name = "go-api" }
-  spec {
-    replicas = 3
-    selector { match_labels = { app = "go" } }
-    template {
-      metadata { labels = { app = "go" } }
-      spec {
-        container {
-          image = "abatistas/cursoci2:latest"
-          name  = "go"
-          port { container_port = 8000 }
-        }
-      }
-    }
-  }
-  # ISSO É VITAL: Impede o Terraform de tentar criar o app 
-  # antes de existirem máquinas reais para recebê-lo.
-  depends_on = [aws_eks_node_group.study_nodes]
-}
-
 # Serviço que expõe a aplicação para a Internet na porta 8000
 resource "kubernetes_service" "LoadBalancer" {
   metadata { name = "load-balancer-go-api" }
@@ -31,7 +9,4 @@ resource "kubernetes_service" "LoadBalancer" {
     }
     type = "LoadBalancer"
   }
-  # Importante: o LB só pode ser criado se os nós estiverem prontos
-  # O Service depende do Deployment, que por sua vez já depende dos nós.
-  depends_on = [kubernetes_deployment.go-api]
 }
